9b221eb277fb9b93779c1dd53d61ba7cf50b07f5
Vincent Molinié <vincent.m@forestadmin.com>
2023-07-28 11:22:21 +0200

    feat: first implementation

---
+++ wait-agent-started.sh
@@ -0,0 +1,28 @@
+#bin/bash
+set -Eeou pipefail;
+
+log_when_setup=$1
+log_when_server_failed=$2
+
+cleanup()
+{
+  echo "Killing agent process"
+  kill $(<pid)
+  sleep 1;
+}
+
+while read input
+do
+  echo $input
+  if echo $input | grep -q $log_when_server_failed;
+  then
+    cleanup;
+    exit 2;
+  else
+    if echo $input | grep -q $log_when_setup;
+    then
+      cleanup;
+      exit 0;
+    fi;
+  fi;
+done
