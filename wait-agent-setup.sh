e941fcfc9e66ea7fb8eba0c497fd07b5214db6be
Vincent Molinié <vincent.m@forestadmin.com>
2023-09-22 11:29:11 +0200

    fix: fix grep

--- wait-agent-setup.sh
+++ wait-agent-setup.sh
@@ -14,12 +14,12 @@ cleanup()
 while read input
 do
   echo $input
-  if echo $input | grep -q $log_when_server_failed;
+  if echo $input | grep -q "$log_when_server_failed";
   then
     cleanup;
     exit 2;
   else
-    if (echo $input | grep -q $log_when_setup) || (echo $input | grep -q "ForestAdmin server failed to find the project");
+    if (echo $input | grep -q "$log_when_setup") || (echo $input | grep -q "ForestAdmin server failed to find the project");
     then
       cleanup;
       exit 0;
